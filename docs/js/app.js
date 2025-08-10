012d66415cec933f31e84838392802ad86768ccb
hanif <hanif_1362@yahoo.com>
2025-08-10 17:26:11 +0200

    Add reset button and custom percent input logic

--- docs/js/app.js
+++ docs/js/app.js
@@ -7,14 +7,15 @@ let inputCustom
 // output
 let tipPerPersonOutput
 let totalTipOutput
-let resetButton 
+let resetButton
 
 let selectedTipButtonWithFixedPercent = null
 
 window.onload = () => {
     billInput = new InputNumber(document.querySelector('#bill-input'), onChangedBillInputData)
-    personInput = new InputNumber(document.querySelector('#person-input'), onChangedBillInputData)
-    inputCustom = new InputCustom(document.querySelector('#custom-input'), onChangedBillInputData) 
+    personInput = new InputNumber(document.querySelector('#person-input'), onChangedPersonInputData)
+    inputCustom = new InputCustom(document.querySelector('#custom-input'), onChangedCustomInputData, onClickCustomInput)
+    resetButton = new ResetButton(document.querySelector('#reset-button'), onResetButtonClicked)
     tipPerPersonOutput = document.querySelector('#total-tip-output')
     totalTipOutput = document.querySelector('#tip-per-person-output')
 
@@ -44,10 +45,10 @@ window.onload = () => {
 
     inputTipsButtonsWithFixedPercent.forEach(tipButtonWithPercentItem => {
         tipButtonWithPercentItem.component.addEventListener('click', () => onTipsButtonWithPercentSelected(tipButtonWithPercentItem))
-    })    
+    })
 }
 
-function onChangedBillInputData() {
+function onChangedPersonInputData() {
     if (personInput.value === '0') {
         personInput.errorText = 'Can\'t be zero'
 
@@ -56,18 +57,19 @@ function onChangedBillInputData() {
         return
     } else {
         personInput.clearError()
+        onChangedBillInputData()
     }
+}
 
-    if (!selectedTipButtonWithFixedPercent && inputCustom.value === 0) {
+function onChangedBillInputData() {
+    if (personInput.value === '0') {
         return
     }
 
-    let tipPercentValue
+    let tipPercentValue = getTipPercentValue()
 
-    if (selectedTipButtonWithFixedPercent) {
-        tipPercentValue = selectedTipButtonWithFixedPercent.value
-    } else {
-        tipPercentValue = inputCustom.value
+    if (!tipPercentValue) {
+        0
     }
 
     const tipTotal = billInput.value * tipPercentValue / 100
@@ -75,6 +77,10 @@ function onChangedBillInputData() {
 
     tipPerPersonOutput.textContent = Math.round(tipPerPerson * 100) / 100
     totalTipOutput.textContent = Math.round(tipTotal * 100) / 100
+
+    if (!isResetButtonDisabled()) {
+        resetButton.enable()
+    }
 }
 
 function onTipsButtonWithPercentSelected(tipButtonWithPercent) {
@@ -82,27 +88,65 @@ function onTipsButtonWithPercentSelected(tipButtonWithPercent) {
         return
     }
 
+    inputCustom.reset()
+
     if (selectedTipButtonWithFixedPercent) {
         selectedTipButtonWithFixedPercent.component.classList.remove('selected')
     }
 
-    //TODO remove styling for custom in an else
-
     selectedTipButtonWithFixedPercent = getTipButtonWithFixedPercentByPercentValue(tipButtonWithPercent.value)
-    
+
     selectedTipButtonWithFixedPercent.component.classList.add('selected')
-    
+
     onChangedBillInputData()
+}
+
+function onChangedCustomInputData() {
+    onChangedBillInputData()
+}
+
+function onClickCustomInput() {
+    if (selectedTipButtonWithFixedPercent) {
+        selectedTipButtonWithFixedPercent.component.classList.remove('selected')
+    }
+
+    selectedTipButtonWithFixedPercent = null
 }
 
 function getTipButtonWithFixedPercentByPercentValue(percentValue) {
     return inputTipsButtonsWithFixedPercent.find(item => item.value === percentValue)
 }
 
+function isResetButtonDisabled() {
+    console.log('isResetButtonDisabled')
+    return !getTipPercentValue() && !personInput.value && !personInput.value
+}
+
 function onResetButtonClicked() {
-    tipPerPersonOutput.text = '0.00'
-    tipTotalOutput.text = '0.00'
-    //TODO complete it
+    personInput.value = 0
+    billInput.value = 0
+
+    if (selectedTipButtonWithFixedPercent) {
+        selectedTipButtonWithFixedPercent.component.classList.remove('selected')
+        selectedTipButtonWithFixedPercent = null
+    }
+
+    inputCustom.reset()
+
+    tipPerPersonOutput.textContent = '0.00'
+    totalTipOutput.textContent = '0.00'
+}
+
+function getTipPercentValue() {
+    if (selectedTipButtonWithFixedPercent) {
+        return selectedTipButtonWithFixedPercent.value
+    }
+
+    if (inputCustom.value) {
+        return inputCustom.value
+    }
+
+    return 0
 }
 
 class InputNumber {
@@ -110,11 +154,11 @@ class InputNumber {
         this.element = element
         this.input = element.getElementsByTagName('input')[0]
         this.errorSpan = element.querySelector('.input-input-error-container')
-        
+
         this.input.addEventListener('change', changeListener)
         this.input.addEventListener('focus', () => this.element.classList.add('focused'))
         this.input.addEventListener('blur', () => this.element.classList.remove('focused'))
-        
+
     }
 
     get value() {
@@ -138,29 +182,33 @@ class InputNumber {
 class InputCustom {
     static SPAN_TEXT = 'Custom'
 
-    constructor(element, changeListener) {
+    constructor(element, changeListener, clickListener) {
         this.element = element
         this.input = element.getElementsByTagName('input')[0]
         this.span = element.getElementsByTagName('span')[0]
         this.changeListener = changeListener
+        this.clickListener = clickListener
 
-        this.element.addEventListener('click', (event) =>this.onClick(event)) 
+        this.element.addEventListener('click', (event) => this.onClick(event))
         this.input.addEventListener('blur', (event) => this.onBlurInput(event))
         this.span.textContent = InputCustom.SPAN_TEXT
     }
 
     onClick() {
-        this.element.classList.add('selected')
+        this.element.classList.add('focused')
         this.input.focus()
+        this.clickListener()
     }
 
     onBlurInput() {
-        this.element.classList.remove('selected')
+        this.element.classList.remove('focused')
 
-        if (this.input.value === 0) {
+        if (this.input.value === '0') {
             this.span.textContent = InputCustom.SPAN_TEXT
+            this.element.classList.remove('selected')
         } else {
             this.span.textContent = this.input.value
+            this.element.classList.add('selected')
         }
 
         this.changeListener()
@@ -173,5 +221,42 @@ class InputCustom {
 
     get value() {
         return this.input.value
+    }
+}
+
+class ResetButton {
+    constructor(element, clickListener) {
+        this.element = element
+        this.button = element.getElementsByTagName('button')[0]
+        this.isDisabled = true
+        this.clickListener = clickListener
+
+        this.disable()
+        this.button.addEventListener('click', event => this.onClick(event))
+        this.button.addEventListener('mouseenter', event => this.onHover(event))
+        this.button.addEventListener('mouseleave', this.element.classList.remove('hover'))
+    }
+
+    onHover(event) {
+        if (!this.isDisabled) {
+            this.element.classList.add('hover')
+        }
+    }
+
+    onClick() {
+        if (!this.isDisabled) {
+            this.clickListener()
+        }
+    }
+
+    enable() {
+        this.isDisabled = false
+        this.element.classList.remove('disabled')
+    }
+
+    disable() {
+        this.isDisabled = true
+        this.element.classList.add('disabled')
+        this.element.classList.remove('hover')
     }
 }
